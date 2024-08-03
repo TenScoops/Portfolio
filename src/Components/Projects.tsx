484a753abad281eea8c76bde1d21f0e2b38c8a47
TenScoops <p.ayres9@gmail.com>
2024-08-03 02:21:20 -0400

    rpoject navigation

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,14 +1,16 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 import { Gamepad, ProgressBar } from "../icons"
 
 const Projects = ()=> {
+    const [section, setSection] = useState<string>("apps")
     return (
-    <div className="h-screen flex flex-col items-center justify-center w-full">
+    <div className="h-screen flex flex-col items-center w-full">
         <div className="flex items-start justify-start w-[1000px] space-x-12">
-            <h1 className="text-xl font-bold underline">Apps</h1>
-            <h1 className="text-xl">Design</h1>
+            <h1 onClick={()=>{setSection("apps")}} className={`text-xl cursor-pointer ${section === "apps" && 'font-bold underline'}`}>Apps</h1>
+            <h1 onClick={()=>{setSection("designs")}} className={`text-xl cursor-pointer  ${section === "designs" && 'font-bold underline'}`}>Designs</h1>
         </div>
-
+        {section === "apps"&&
         <div className="flex flex-row items-center justify-center space-x-28 mt-16">
             <Link to="/projects/lifexp" 
                   className="flex items-center justify-center shadow-sharp-lg hover:shadow-sharp-2xl w-[470px] h-[470px] rounded-3xl cursor-pointer shadow-transition duration-200 icon-hover3">
@@ -18,14 +20,18 @@ const Projects = ()=> {
                   className="flex items-center justify-center shadow-sharp-lg hover:shadow-sharp-2xl w-[470px] h-[470px] rounded-3xl cursor-pointer shadow-transition duration-200 icon-hover3">
                 <ProgressBar />
             </Link>
-        </div>
-        <div className="flex flex-row items-center justify-center space-x-3 mt-10">
-            
-            
-            
-            
-        </div>
+        </div>}
 
+        {section ==="designs"&&
+        <div className="flex flex-row space-x-28 mt-16">
+            <Link to="/projects/lifexp" 
+                className="flex items-center justify-center shadow-sharp-lg hover:shadow-sharp-2xl w-[470px] h-[470px] rounded-3xl cursor-pointer shadow-transition duration-200 icon-hover3">
+                <Gamepad />
+            </Link>
+        
+        </div>}
+        
+       
         {/* <div className="flex flex-row items-center justify-center space-x-3 mt-8">
             <img className="rounded-lg border-2 border-black" src={dwarf} height={400} width={400}/>
             <div className="w-1/3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam, eligendi quos! Cum odit porro laborum quam quia totam accusantium cupiditate suscipit eaque obcaecati quaerat repudiandae, dicta ullam ad iste officia.
