e876f2d5e341fedffd104ccfc669405d44e58130
TenScoops <p.ayres9@gmail.com>
2024-09-22 01:19:15 -0400

    change to label on projects

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -16,12 +16,12 @@ const Projects = ()=> {
             <Link to="/projects/lifexp" 
                   className="flex flex-col items-center justify-center shadow-sharp-md hover:shadow-sharp-2xl w-80 h-80 sm:w-[420px] sm:h-[420px] xl:w-[470px] xl:h-[470px] space-y-3 rounded-3xl cursor-pointer shadow-transition duration-200 icon-hover3">
                 <Gamepad />
-                <h1 className="italic">Click for more info</h1>
+                <h1 className="italic">Click for info on project</h1>
             </Link>
             <Link to="projects/pomoprogress"
                   className="flex flex-col items-center justify-center shadow-sharp-md hover:shadow-sharp-2xl w-80 h-80 sm:w-[420px] sm:h-[420px] xl:w-[470px] xl:h-[470px] space-y-2 rounded-3xl cursor-pointer shadow-transition duration-200 icon-hover3">
                 <ProgressBar />
-                <h1 className="italic">Click for more info</h1>
+                <h1 className="italic">Click for info on project</h1>
             </Link>
         </div>}
 
@@ -30,7 +30,7 @@ const Projects = ()=> {
             <Link to="/projects/imaginai" 
                 className="flex flex-col items-center justify-center shadow-sharp-md hover:shadow-sharp-2xl w-80 h-80 sm:w-[420px] sm:h-[420px] xl:w-[470px] xl:h-[470px] rounded-3xl space-y-2 cursor-pointer shadow-transition duration-200 icon-hover3">
                 <AiIcon />
-                <h1 className="italic">Click for more info</h1>
+                <h1 className="italic">Click for info on project</h1>
             </Link>
         
         </div>}
