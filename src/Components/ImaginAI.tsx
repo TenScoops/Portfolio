848332815179b9a23655b1825f9ee45d5e4b56f7
TenScoops <p.ayres9@gmail.com>
2024-10-04 13:16:31 -0400

    animation changes 4

--- src/Components/ImaginAI.tsx
+++ src/Components/ImaginAI.tsx
@@ -16,7 +16,7 @@ const ImaginAi:React.FC<ImaginAIProps> = ({handleSectionClick}) =>{
         },50)
     }
 
-    return(<div className="flex flex-col justify-center items-center h-screen">
+    return(<div className="flex flex-col justify-center items-center h-screen slide-right">
         <div className="w-[345px] sm0:w-[420px] sm:w-[470px] lg:w-[1030px]">
             <button className="border border-black rounded-md h-12 w-12 flex items-center justify-center shadow-sharp-sm hover:shadow-sharp-md icon-hover3"
                     onClick={handleButtonClick}>
