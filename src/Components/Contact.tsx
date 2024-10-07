0b4bb95cf59ff733357e7ed754841b62e36164a0
TenScoops <p.ayres9@gmail.com>
2024-10-07 12:18:00 -0400

    resolving extension errors

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 // import HangingAstronaut from '../assets/Mail_Rocket.png';
 // import Linemail from "../assets/linemail.webp";
-import Helmet from "../assets/helmet.webp";
+import Helmet from "../assets/helmet.jpg";
 import { CopyIcon } from '../icons';
 
 const Contact = () => {
