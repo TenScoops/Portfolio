fd0e883bd656fb0f37ecffec1f985ef09e178248
TenScoops <p.ayres9@gmail.com>
2024-10-14 22:57:40 -0400

    description change 2

--- src/Components/LifeXP.tsx
+++ src/Components/LifeXP.tsx
@@ -22,7 +22,7 @@ const paragraph = () =>{
 }
 const approach = () =>{
     return <p className=" "> 
-                Design from ground up and program an app that can <strong>improve the lives</strong> of many through an all-encompassing, intuitive, and engaging approach.
+                Design from ground up and program an app that can <strong>improve people's mental and physical health</strong> through an all-encompassing, intuitive, and engaging approach.
             </p>
 }
 
